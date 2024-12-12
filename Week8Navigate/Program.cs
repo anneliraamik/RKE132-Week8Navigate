10136264573254c498c0718fd9df808c2e9ac63c
Anneli Raamik. <anneli.raamik@tktk.ee>
2024-12-12 20:57:09 +0200

    add Week8Navigate code

---
+++ Week8Navigate/Program.cs
@@ -0,0 +1,28 @@
+﻿string  folderPath = @"C:\Users\an13r\source\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+
+//string[] heroes = {"Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Jokker", "Sauron" };
+
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego brick"};
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string Villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {Villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex  = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
